ee6ec800be5aef6baba526f04abbdd8dd70277d4
hiepvo01 <vohi0311@gmail.com>
2023-03-14 16:05:03 +1100

    MNIST

--- reconstructMNIST.py
+++ reconstructMNIST.py
@@ -4,191 +4,320 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from util import EarlyStopping, MetricMonitor
 from util import TwoCropTransform, SupCon, SupConLoss, save_model
-from simclrMNIST import Encoder
+import numpy as np
+from torch.utils.data import DataLoader,random_split
 
-class Decoder(torch.nn.Module):
-    "Decoder network"
-    def __init__(self):
-        super(Decoder, self).__init__()
-        # L1 (?, 28, 28, 1) -> (?, 28, 28, 32) -> (?, 14, 14, 32)
-        self.layer1 = torch.nn.Sequential(
-            torch.nn.ConvTranspose2d(128, 64, kernel_size=3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(64),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(p=0.2)
-            )
-        # L2 (?, 14, 14, 32) -> (?, 14, 14, 64) -> (?, 7, 7, 64)
-        self.layer2 = torch.nn.Sequential(
-            torch.nn.Upsample(scale_factor=2),
-            torch.nn.ConvTranspose2d(64, 32, kernel_size=3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(32),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(p=0.2)
-            )
-        # L3 (?, 7, 7, 64) -> (?, 7, 7, 128) -> (?, 4, 4, 128)
-        self.layer3 = torch.nn.Sequential(
-            torch.nn.Upsample(scale_factor=2),
-            torch.nn.ConvTranspose2d(32, 1, kernel_size=3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(1),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(p=0.2)
-            )
-        self._to_linear = 7 * 7 * 128
+# Commented out IPython magic to ensure Python compatibility.
+## Standard libraries
+from copy import deepcopy
 
+## Imports for plotting
+plt.set_cmap('cividis')
+# %matplotlib inline
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg', 'pdf') # For export
+import matplotlib
+matplotlib.rcParams['lines.linewidth'] = 2.0
+import seaborn as sns
+sns.set()
+
+## tqdm for loading bars
+from tqdm.notebook import tqdm
+
+## PyTorch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+
+## Torchvision
+import torchvision
+from torchvision.datasets import MNIST
+from torchvision import transforms
+
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+# PyTorch Lightning
+try:
+    import pytorch_lightning as pl
+except ModuleNotFoundError: # Google Colab does not have PyTorch Lightning installed by default. Hence, we do it here if necessary
+    #!pip install --quiet pytorch-lightning>=1.4
+    import pytorch_lightning as pl
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+
+# Import tensorboard
+# %load_ext tensorboard
+
+# Path to the folder where the datasets are/should be downloaded (e.g. MNIST)
+DATASET_PATH = "./data"
+# Path to the folder where the pretrained models are saved
+CHECKPOINT_PATH = "../results/MNIST"
+# In this notebook, we use data loaders with heavier computational processing. It is recommended to use as many
+# workers as possible in a data loader, which corresponds to the number of CPU cores
+NUM_WORKERS = os.cpu_count()
+
+# Setting the seed
+pl.seed_everything(42)
+
+# Ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+print("Device:", device)
+print("Number of workers:", NUM_WORKERS)
+
+data_dir = './data'
+
+train_dataset = torchvision.datasets.MNIST(data_dir, train=True, download=True)
+test_dataset  = torchvision.datasets.MNIST(data_dir, train=False, download=True)
+
+train_transform = transforms.Compose([
+transforms.ToTensor(),
+])
+
+test_transform = transforms.Compose([
+transforms.ToTensor(),
+])
+
+train_dataset.transform = train_transform
+test_dataset.transform = test_transform
+
+m=len(train_dataset)
+
+train_data, val_data = random_split(train_dataset, [int(m-m*0.2), int(m*0.2)])
+batch_size=256
+
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
+valid_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,shuffle=True)
+
+class SimCLR(pl.LightningModule):
+    
+    def __init__(self, hidden_dim, lr, temperature, weight_decay, max_epochs=500, c_hid=32):
+        super().__init__()
+        self.save_hyperparameters()
+        assert self.hparams.temperature > 0.0, 'The temperature must be a positive float!'
+        # Base model f(.)
+        self.convnet = torchvision.models.resnet18(num_classes=4*hidden_dim)  # Output of last linear layer
+        self.convnet.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+
+        # The MLP for g(.) consists of Linear->ReLU->Linear 
+        self.convnet.fc = nn.Sequential(
+            self.convnet.fc,  # Linear(ResNet output, 4*hidden_dim)
+            nn.ReLU(inplace=True),
+            nn.Linear(4*hidden_dim, hidden_dim)
+        )
+        
     def forward(self, x):
-        x = x.view(-1, 128, 7, 7) # Flatten them for FC
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
+        return self.convnet(x)
+    
+
+    def configure_optimizers(self):
+        optimizer = optim.AdamW(self.parameters(), 
+                                lr=self.hparams.lr, 
+                                weight_decay=self.hparams.weight_decay)
+        lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                            T_max=self.hparams.max_epochs,
+                                                            eta_min=self.hparams.lr/50)
+        return [optimizer], [lr_scheduler]
+        
+    def info_nce_loss(self, batch, mode='train'):
+        imgs, _ = batch
+        imgs = torch.cat(imgs, dim=0)
+        
+        # Encode all images
+        feats = self.convnet(imgs)
+        # Calculate cosine similarity
+        cos_sim = F.cosine_similarity(feats[:,None,:], feats[None,:,:], dim=-1)
+        # Mask out cosine similarity to itself
+        self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=cos_sim.device)
+        cos_sim.masked_fill_(self_mask, -9e15)
+        # Find positive example -> batch_size//2 away from the original example
+        pos_mask = self_mask.roll(shifts=cos_sim.shape[0]//2, dims=0)
+        # InfoNCE loss
+        cos_sim = cos_sim / self.hparams.temperature
+        nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
+        nll = nll.mean()
+        
+        # Logging loss
+        self.log(mode+'_loss', nll)
+        # Get ranking position of positive example
+        comb_sim = torch.cat([cos_sim[pos_mask][:,None],  # First position positive example
+                              cos_sim.masked_fill(pos_mask, -9e15)], 
+                             dim=-1)
+        sim_argsort = comb_sim.argsort(dim=-1, descending=True).argmin(dim=-1)
+        # Logging ranking metrics
+        self.log(mode+'_acc_top1', (sim_argsort == 0).float().mean())
+        self.log(mode+'_acc_top5', (sim_argsort < 5).float().mean())
+        self.log(mode+'_acc_mean_pos', 1+sim_argsort.float().mean())
+        
+        return nll
+        
+    def training_step(self, batch, batch_idx):
+        return self.info_nce_loss(batch, mode='train')
+        
+    def validation_step(self, batch, batch_idx):
+        self.info_nce_loss(batch, mode='val')
+
+class Decoder(nn.Module):
+    
+    def __init__(self, encoded_space_dim=4):
+        super().__init__()
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(encoded_space_dim, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 3 * 3 * 32),
+            nn.ReLU(True)
+        )
+
+        self.unflatten = nn.Unflatten(dim=1, 
+        unflattened_size=(32, 3, 3))
+
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, 
+            stride=2, output_padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, 
+            padding=1, output_padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 3, stride=2, 
+            padding=1, output_padding=1)
+        )
+        
+    def forward(self, x):
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
         return x
 
-def decoder_training(epoch, model, decoder, train_loader, optimizer, criterion):
-    "Training over an epoch"
-    metric_monitor = MetricMonitor()
-    model.eval()
+### Define the loss function
+loss_fn = torch.nn.MSELoss()
+
+### Define an optimizer (both for the encoder and the decoder!)
+lr= 0.001
+
+### Set the random seed for reproducible results
+torch.manual_seed(0)
+
+### Initialize the two networks
+d = 128
+
+#model = Autoencoder(encoded_space_dim=encoded_space_dim)
+encoder = SimCLR( 
+            hidden_dim=128, 
+            lr=5e-4, 
+            temperature=0.07, 
+            weight_decay=1e-4, 
+            max_epochs=100)
+
+encoder.convnet.load_state_dict(
+    torch.load('./results/simclrMNIST.pt')
+)
+
+decoder = Decoder(encoded_space_dim=d)
+params_to_optimize = [
+    {'params': encoder.parameters()},
+    {'params': decoder.parameters()}
+]
+
+optim = torch.optim.Adam(params_to_optimize, lr=lr, weight_decay=1e-05)
+
+# Check if the GPU is available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(f'Selected device: {device}')
+
+# Move both the encoder and the decoder to the selected device
+encoder.to(device)
+decoder.to(device)
+
+### Training function
+def train_epoch(encoder, decoder, device, dataloader, loss_fn, optimizer):
+    # Set train mode for both the encoder and the decoder
+    encoder.train()
     decoder.train()
-    for batch_idx, (data,labels) in enumerate(train_loader):
-        if torch.cuda.is_available():
-            data,labels = data.cuda(), labels.cuda()
-        data = torch.autograd.Variable(data,False)
-        with torch.no_grad():
-            features = model.encoder(data)
-        output = decoder(features.float())
-        loss = criterion(output, data)
-        metric_monitor.update("Loss", loss.item())
-        data.detach()
+    train_loss = []
+    # Iterate the dataloader (we do not need the label values, this is unsupervised learning)
+    for image_batch, _ in dataloader: # with "_" we just ignore the labels (the second element of the dataloader tuple)
+        # Move tensor to the proper device
+        image_batch = image_batch.to(device)
+        # Encode data
+        encoded_data = encoder(image_batch)
+        # Decode data
+        decoded_data = decoder(encoded_data)
+        # Evaluate loss
+        loss = loss_fn(decoded_data, image_batch)
+        # Backward pass
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    print("[Epoch: {epoch:03d}] Train      | {metric_monitor}".format(epoch=epoch, metric_monitor=metric_monitor))
-    return metric_monitor.metrics['Loss']['avg']
+        # Print batch loss
+        print('\t partial train loss (single batch): %f' % (loss.data))
+        train_loss.append(loss.detach().cpu().numpy())
 
+    return np.mean(train_loss)
 
-def decoder_validation(epoch, model, decoder, valid_loader, criterion):
-    "Validation over an epoch"
-    metric_monitor = MetricMonitor()
-    model.eval()
+### Testing function
+def test_epoch(encoder, decoder, device, dataloader, loss_fn):
+    # Set evaluation mode for encoder and decoder
+    encoder.eval()
     decoder.eval()
-    with torch.no_grad():
-        for batch_idx, (data,labels) in enumerate(valid_loader):
-            if torch.cuda.is_available():
-                data,labels = data.cuda(), labels.cuda()
-            data = torch.autograd.Variable(data,False)
-            features = model.encoder(data)
-            output = decoder(features.float())
-            loss = criterion(output,data) 
-            metric_monitor.update("Loss", loss.item())
-            data.detach()
-    print("[Epoch: {epoch:03d}] Validation | {metric_monitor}".format(epoch=epoch, metric_monitor=metric_monitor))
-    return metric_monitor.metrics['Loss']['avg']
+    with torch.no_grad(): # No need to track the gradients
+        # Define the lists to store the outputs for each batch
+        conc_out = []
+        conc_label = []
+        for image_batch, _ in dataloader:
+            # Move tensor to the proper device
+            image_batch = image_batch.to(device)
+            # Encode data
+            encoded_data = encoder(image_batch)
+            # Decode data
+            decoded_data = decoder(encoded_data)
+            # Append the network output and the original image to the lists
+            conc_out.append(decoded_data.cpu())
+            conc_label.append(image_batch.cpu())
+        # Create a single tensor with all the values in the lists
+        conc_out = torch.cat(conc_out)
+        conc_label = torch.cat(conc_label) 
+        # Evaluate global loss
+        val_loss = loss_fn(conc_out, conc_label)
+    return val_loss.data
 
-
-def main():
+def plot_ae_outputs(encoder,decoder,n=10):
+    plt.figure(figsize=(16,4.5))
+    targets = test_dataset.targets.numpy()
+    t_idx = {i:np.where(targets==i)[0][0] for i in range(n)}
+    for i in range(n):
+      ax = plt.subplot(2,n,i+1)
+      img = test_dataset[t_idx[i]][0].unsqueeze(0).to(device)
+      encoder.eval()
+      decoder.eval()
+      with torch.no_grad():
+         rec_img  = decoder(encoder(img))
+      plt.imshow(img.cpu().squeeze().numpy(), cmap='gist_gray')
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)  
+      if i == n//2:
+        ax.set_title('Original images')
+      ax = plt.subplot(2, n, i + 1 + n)
+      plt.imshow(rec_img.cpu().squeeze().numpy(), cmap='gist_gray')  
+      ax.get_xaxis().set_visible(False)
+      ax.get_yaxis().set_visible(False)  
+      if i == n//2:
+         ax.set_title('Reconstructed images')
+    plt.show()
     
-    num_epochs = 100
-    use_early_stopping = True
-    use_scheduler = True
-    head_type = 'mlp' # choose among 'mlp' and 'linear"
-    method = 'SimCLR' # choose among 'SimCLR' and 'SupCon'
-    save_file = os.path.join('./results/', 'simclrMNIST.pth')
-    if not os.path.isdir('./results/'):
-         os.makedirs('./results/')
-    
-    train_transform = transforms.Compose([
-                                       transforms.RandomHorizontalFlip(),
-                                       transforms.ToTensor(),
-                                       transforms.Normalize((0.5,), (0.5,)),
-                                       ])
-    valid_transform = transforms.Compose([
-                                       transforms.ToTensor(),
-                                       transforms.Normalize((0.5,), (0.5,)),
-                                       ])
-    
-    train_set = datasets.MNIST('./data', download=True, train=True, transform=train_transform)
-    valid_set = datasets.MNIST('./data', download=True, train=False, transform=valid_transform)
-    
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
-    valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=64, shuffle=True)
-    
-    # Part 2
-    encoder = Encoder()
-    model = SupCon(encoder, head=head_type, feat_dim=128)
-    decoder = Decoder()
-    criterion = torch.nn.MSELoss()
-    
-    ckpt = torch.load(save_file, map_location='cpu')
-    state_dict = ckpt['model']
-    new_state_dict = {}
-    for k, v in state_dict.items():
-        k = k.replace("module.", "")
-        new_state_dict[k] = v
-    state_dict = new_state_dict
-    model.load_state_dict(state_dict)
-    
-    
-    if torch.cuda.is_available():
-        model = model.cuda()
-        decoder = decoder.cuda()
-        criterion = criterion.cuda()
-        
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-3)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.9)
-        
-    train_losses = []
-    valid_losses = []
-    
-    if use_early_stopping:
-        early_stopping = EarlyStopping(patience=30, verbose=False, delta=1e-4)
- 
-    for epoch in range(1, num_epochs+1):
-        
-        train_loss = decoder_training(epoch, model, decoder, train_loader, optimizer, criterion)
-        valid_loss = decoder_validation(epoch, model, decoder, valid_loader, criterion)
-
-        
-        if use_scheduler:
-            scheduler.step()
-            
-        train_losses.append(train_loss)
-        valid_losses.append(valid_loss)
-             
-        if use_early_stopping: 
-            early_stopping(valid_loss, model)
-            
-            if early_stopping.early_stop:
-                print('Early stopping at epoch', epoch)
-                #model.load_state_dict(torch.load('checkpoint.pt'))
-                break
-    
-    save_model(decoder, optimizer, num_epochs, './results/decoderMNIST.pth')
-
-     
-    plt.plot(range(1,len(train_losses)+1), train_losses, color='b', label = 'training loss')
-    plt.plot(range(1,len(valid_losses)+1), valid_losses, color='r', linestyle='dashed', label = 'validation loss')
-    plt.legend(), plt.ylabel('loss'), plt.xlabel('epochs'), plt.title('Loss'), plt.show()
-
-
-    counts = 0
-    for batch_idx, (data,labels) in enumerate(train_loader):
-        if counts == 3:
-            break
-        if torch.cuda.is_available():
-            data,labels = data.cuda(), labels.cuda()
-        data, labels = torch.autograd.Variable(data,False), torch.autograd.Variable(labels)
-        with torch.no_grad():
-            features = model.encoder(data)
-        output = decoder(features.float())
-        counts += 1
-        
-        data = data.cpu().detach().numpy().reshape(-1, 28, 28)
-        plt.imshow(data[0])
-        plt.show()
-        output = output.cpu().detach().numpy().reshape(-1, 28, 28)
-        plt.imshow(output[0])
-        plt.show()
-
-        
-    
-if __name__ == "__main__":
-    main()
-
+num_epochs = 30
+diz_loss = {'train_loss':[],'val_loss':[]}
+for epoch in range(num_epochs):
+   train_loss =train_epoch(encoder,decoder,device,
+   train_loader,loss_fn,optim)
+   val_loss = test_epoch(encoder,decoder,device,test_loader,loss_fn)
+   print('\n EPOCH {}/{} \t train loss {} \t val loss {}'.format(epoch + 1, num_epochs,train_loss,val_loss))
+   diz_loss['train_loss'].append(train_loss)
+   diz_loss['val_loss'].append(val_loss)
+   plot_ae_outputs(encoder,decoder,n=10)
